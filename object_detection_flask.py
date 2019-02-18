cf0560203dbb207f08cae1d4a40c583ca58c7777
houssem ben mahfoudh <houcembenmahfoudh@gmail.com>
2019-02-18 11:46:25 +0100

    add flask server

---
+++ object_detection_flask.py
@@ -0,0 +1,60 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from flask import Flask
+from flask import jsonify
+import cv2
+
+app = Flask(__name__)
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+  # Loading label map
+  # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+@app.route('/')
+def index():
+  cap = cv2.VideoCapture(1)
+  with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        ret,image_np = cap.read()
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        (boxes, scores, classes, num_detections) = sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        display_str = vis_util.labels_on_image_array(np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),np.squeeze(scores),category_index,min_score_thresh=0.3)
+  return jsonify(display_str)
+
+if __name__ == '__main__':
+  app.run(debug=True)
